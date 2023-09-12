236709046dfaee2977663e5362b29e5c1cc7686c
aliceco <alicecohen.r@gmail.com>
2023-09-12 11:15:40 +0200

    instruktioner till uppgift 3

--- Labb3/main.py
+++ Labb3/main.py
@@ -1,5 +1,6 @@
 from bintreeFile import Bintree
 """
+uppgift 1
 def makeTree():
     tree = Bintree()
     data = input("Mata in i träd: ").strip()
@@ -24,7 +25,8 @@ def main():
 
 main()
 """
-
+"""
+Uppgift 2
 svenska = Bintree()
 with open("Labb3/word3.txt", "r", encoding = "utf-8") as svenskfil:
     for rad in svenskfil:
@@ -34,3 +36,29 @@ with open("Labb3/word3.txt", "r", encoding = "utf-8") as svenskfil:
         else:
             svenska.put(ordet)             # in i sökträdet
 print("\n")
+"""
+
+#Uppgift 3
+
+with open("Labb3/engelska.txt", "r", encoding= "utf-8") as engelskfil:
+    for rad in engelskfil:
+        print(rad.strip())
+
+"""
+engelska filen
+Måste ta bort tomma rader
+måste separera orden baserat på split
+måste ta bort "-tecken och punkter och '-tecken och !-tecken (alltså en jävla massa tecken)
+"""
+
+"""
+vi ska använda uppgift 2
+vi får ord - kollar om det finns i engelska ordlistan
+om nej - stoppa in
+om ja - kolla om det oxå finns i svenska
+    om ja - kolla om finns i utskriften
+        om ja - ingenting
+        om nej - skriv ut
+    om nej - ingenting
+
+"""
