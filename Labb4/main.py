b92ff46e2672acd2ddbaaecc49afd2498bc2558c
aliceco <alicecohen.r@gmail.com>
2023-09-22 08:53:06 +0200

    kommenterade

--- Labb4/main.py
+++ Labb4/main.py
@@ -31,47 +31,48 @@ from linkedQFile import LinkedQ
 #uppgift 2
 
 
-def makechildren(startord,slutord,q, svenska,gamla):
-    gamla.put(startord)
+svenska = Bintree()
+gamla = Bintree()
 
-    for i in range(len(startord)):
+#lägger in filen i ett biinärträd
+with open("Labb4/word3.txt", "r", encoding = "utf-8") as svenskfil: 
+    for rad in svenskfil:
+        ordet = rad.strip()                # Ett trebokstavsord per rad
+        if ordet in svenska:
+            pass
+        else:
+            svenska.put(ordet)     
+
+def makechildren(startord,slutord,q):
+    gamla.put(startord) #lägger in startordet i binära trädet med dum barn
+    for i in range(len(startord)): #loopar genom längden av startord
         alfabet = "abcdefghijklmnopqrstuvwxyzåäö"
-        for letter in alfabet:
+        for letter in alfabet: #byter ut en bokstav i taget
             temp = list(startord)
             temp[i] = letter
             word = ''.join(temp)
-            if word in svenska and word not in gamla: #söker genom svenska (binärträdet)
-                if word == slutord:
-                    return True
-                else:
-                    gamla.put(word)
+            if word in svenska and word not in gamla: # kollar om ordet finns i svenska eller gamla trädet
+                if word == slutord: #om årdet gör det returnera true
+                    return True 
+                else: #Annars lägg i in i gamla och i kön
+                    gamla.put(word) 
                     q.enqueue(word) #lägger till ordet i kön
+                    print(word)
            
 def main():
-    start_word = input("Skriv ett startord: ").lower()
-    end_word = input("Skriv ett slutord: ").lower()
-
-    svenska = Bintree()
-    gamla = Bintree()
-
-    with open("Labb4/word3.txt", "r", encoding = "utf-8") as svenskfil: 
-        for rad in svenskfil:
-            ordet = rad.strip()                # Ett trebokstavsord per rad
-            if ordet in svenska:
-                pass
-            else:
-                svenska.put(ordet)     
-
+    startord_input = input("Skriv ett startord: ").lower()
+    slutord_input = input("Skriv ett slutord: ").lower()
     # makechildren(start_word)
-    #skapa kön q
-    q = LinkedQ()
-    q.enqueue(start_word)
-    while not q.isEmpty():
-        startord = q.dequeue()
-        if makechildren(startord, end_word, q, svenska, gamla) is True:
-            print(f"Det finns en väg till {end_word}")
-            break
-        elif q.isEmpty():
-            print(f"Det finns ingen väg till {end_word}")
 
+    q = LinkedQ()
+    q.enqueue(startord_input) #lägger in startordet i kö
+    while not q.isEmpty():
+        startord = q.dequeue() #tar ut ur kön och sparar värdet
+        if makechildren(startord, slutord_input, q): #kallar på metoden
+            print(f"Det finns en väg till {slutord_input}")
+            break
+        elif q.isEmpty(): #om kön är tom så finns det ingen väg
+            print(f"Det finns ingen väg till {slutord_input}")
+
+        
 main()
