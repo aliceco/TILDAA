25453c1de68fef663d12670df6f54830d1f6aee9
aliceco <alicecohen.r@gmail.com>
2023-10-09 15:45:18 +0200

    what

--- Labb7/test.py
+++ Labb7/test.py
@@ -7,8 +7,14 @@ class DictHash:
     def store(self, key, value):
         self.dict[key] = value
     
-    def __getitem__(self, key):
+    def search(self, key):
         return self.dict[key]
+        
+    def __getitem__(self, key):
+        if self.__contains__(key) is True:
+            return self.search(key)
+        else: 
+            return f"{key} is not in file"
     
     def __contains__(self, key):
         if key in self.dict:
@@ -17,21 +23,18 @@ class DictHash:
             return False
 
 
-"""
-kdrama_dict = DictHash()
-def read_file():
+def read_file(kdrama_dict):
     with open('Labb7/kdrama.csv', 'r') as csvfile:
         csvreader = csv.reader(csvfile)
         next(csvreader)
         for row in csvreader:
             kdrama_dict.store(row[0], row[1])
+    return kdrama_dict
 
-read_file()
-print(kdrama_dict["Legend of the Blue Sea"])
-print("hello" in kdrama_dict)
-"""
+def main():
+    kdrama_dict = DictHash()
+    dictt = read_file(kdrama_dict)
+    print(dictt["heja"])
+    print("hello" in kdrama_dict)
 
-
-
-
-    
+main()
