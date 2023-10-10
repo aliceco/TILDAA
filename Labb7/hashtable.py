db3f38edaf99b01d472844a263014fbf81127a1a
aliceco <alicecohen.r@gmail.com>
2023-10-10 15:02:53 +0200

    Labb7 färdig?

---
+++ Labb7/hashtable.py
@@ -0,0 +1,46 @@
+class HashNode:
+    """
+    Noder till klassen Hashtable 
+    """
+
+    def __init__(self, key = "", data = None, pointer = None):
+        self.key = key
+        self.data = data
+        self.pointer = pointer #pointer, points on child
+
+#Fyll i kod här nedan för att initiera hashtabellen
+
+class Hashtable:
+
+    def __init__(self, size):
+        self.size = size #can be used to tell how many objects in table
+        self.table = [None] * size #creates a table with capacity number of elements and all elements are set to None
+        self.collisions = []
+
+
+    def store(self, key, data):
+        index = self.hashfunction(key) 
+        if self.table[index] is None: 
+            self.table[index] = HashNode(key, data)
+        else:
+            self.collisions.append(index)
+            existing_node = self.table[index]
+            self.table[index] = HashNode(key, data)
+            self.table[index].pointer = existing_node
+        
+    def search(self, key):
+        index = self.hashfunction(key) #calculates index of the input
+        element = self.table[index] #saves whats on that index into a variable 
+        while element is not None: #checks if it's empty
+            if element.key == key: #if the key of the element is the same as the ipnut key
+                return element.data #return the data of the element
+            element = element.pointer #otherwise check the next 
+
+        raise KeyError
+
+    def hashfunction(self, key):
+        hash_num = ""
+        for i in key[::-1]:
+            hash_num += str(len(key)*27 + ord(i)**2)
+
+        return int(hash_num) % self.size #returnerar index
--- Labb7/main.py
+++ Labb7/main.py
@@ -1,97 +1,30 @@
-class HashNode:
-    """
-    Noder till klassen Hashtable 
-    """
+from hashtable import Hashtable
+import csv
 
-    def __init__(self, key = "", data = None, pointer = None):
-        """key är nyckeln som anvands vid hashningen
-            data är det objekt som ska hashas in"""
-        self.key = key
-        self.data = data
-        self.pointer = pointer #pointer, points on child
-
-#Fyll i kod här nedan för att initiera hashtabellen
-
-class Hashtable:
-
-    def __init__(self, size):
-        """size: hashtabellens storlek"""
-        self.size = size #can be used to tell how many objects in table
-        self.table = [None] * size #creates a table with capacity number of elements and all elements are set to None
-        self.collisions = 0
-
-
-    def store(self, key, data):
-        """ 
-        key är nyckeln
-        data är objektet som ska lagras
-        Stoppar in "data" med nyckeln "key" i tabellen.
-        """    
-        index = self.hashfunction(key) 
-        print(index)
-        if self.table[index] is None: 
-            self.table[index] = HashNode(key, data)
-        else:
-            """
-            Lägger in den nya saken längst fram och flyttar bak de gamla sakerna
-            så det som las in först kommer ligga "längst bak" i linked list
-            """
-            self.collisions += 1
-            existing_node = self.table[index]
-            self.table[index] = HashNode(key, data)
-            self.table[index].pointer = existing_node
-        
-    def search(self, key):
-        """key är nyckeln
-            Hamtar det objekt som finns lagrat med nyckeln "key" och returnerar det.
-            Om "key" inte finns ska det bli KeyError """
-        #Fyll i kod här!
-        index = self.hashfunction(key) 
-        node = self.table[index]
-        while node is not None:
-            if node.key is key:
-                return node
-            node = node.pointer
-        raise KeyError
-
-    def hashfunction(self, key):
-        """key är nyckeln
-            Beräknar hashfunktionen för key"""
-        #Fyll i kod här!
-        hash_num = ""
-        for i in key[::-1]:
-            hash_num += str(len(key)*1117 + ord(i)**2)
-
-        print(hash_num)
-        return int(hash_num) % self.size #returnerar index
-
-#size = kdrama.size  * 2  (skapa en dubbelt så stor tabell som draman)
-# obj_hash = Hashtable()
-# obj_hash.store(key, value)
 
 def main():
-    hashtable = None
+    kdrama_dict = {}
+    with open('Labb7/kdrama.csv', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)
+        for row in csvreader:
+            kdrama_dict[row[0]] = row[1]
+           
+    tabell = Hashtable(len(kdrama_dict)*2)
+    for title, data in kdrama_dict.items():
+        tabell.store(title, data)
     
-    while True:
-        line = input()
-        key, *value = line.split()
-        if key == '#':
-            print('#')
-            break
-        elif key == 'init' and len(value) > 0:
-            size = int(value[0])
-            hashtable = Hashtable(size)
-            print('New size:', size)
-        elif len(value) > 0:
-            hashtable.store(key, value[0])
-            print(key, '<-', value[0])
-        else:
-            try:
-                value = hashtable.search(key)
-                print(f'{key}: {value}')
-            except KeyError:
-                print('KeyError:', key)
+    print(f"{len(kdrama_dict)} element")
+    print(f"{tabell.collisions} krockar")
+    
+    krockar_lst = tabell.collisions
+    krockar_lst.sort()
+    print(krockar_lst)
+   
+    for i in range(len(krockar_lst)):
+        print(krockar_lst[i])
+        if krockar_lst[i] == krockar_lst[i-1]:
 
 
-if __name__ == "__main__":
-    main()
+
+main()
